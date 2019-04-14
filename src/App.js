8fd5118152aceb6d3b0a6684ed97e2cd0b499373
Don Ward <don.ward.peng@gmail.com>
2019-04-14 19:40:26 -0400

    Added setState and updating and deleting todo items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Components/Todos';
+import Todos from './Components/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+    {
+      id: 1,
+      title: 'Take out the trash',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Dinner with wife',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Meeting with boss',
+      completed: false
+    }
+    ]
+  }
+
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+
+  //Toggle Complete
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id == id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+
+    })})
+    console.log(id)
+  }
   render() {
+    console.log(this.state.todos)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
   }
